2216036f031f6b69a36f142e5dc5657f3e861d88
Roland Katsekpor <RolandKatsekpor@outlook.com>
2025-08-21 18:21:59 -0500

    new updates added

--- CICD.sh
+++ CICD.sh
@@ -7,7 +7,8 @@ VERSION=INSECURE
 #BUILD: Clenaup old container/build image
 docker rm -f $APP_NAME 2>/dev/null || true
 docker build -t $APP_NAME:$VERSION . 
-
+docker scout cves mywebapp:INSECURE --output ./vulns.report
+docker scout cves mywebapp:INSECURE --only-severity high --exit-code
 #TEST: Run the container
 docker run -d -p 80:80 --name $APP_NAME:$VERSION webapp
 
